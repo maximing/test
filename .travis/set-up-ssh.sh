32d8decd027aa4e522c744c37606204cc9b33e0c
Maxim Ignatov <error25@mail.ru>
2018-03-20 20:28:20 +0600

    test

--- .travis/set-up-ssh.sh
+++ .travis/set-up-ssh.sh
@@ -2,4 +2,4 @@
 openssl aes-256-cbc -K $encrypted_98f936accb6c_key -iv $encrypted_98f936accb6c_iv -in .travis/deploy_key.enc -out .travis/deploy_key -d
 chmod 600 .travis/deploy_key
 mv .travis/deploy_key ~/.ssh/id_rsa
-cat .travis/ssh_config >> ~/.ssh/config
+#cat .travis/ssh_config >> ~/.ssh/config
