91c90ae106f66ad37379c8b5bdc5a533e181add5
Maxim Ignatov <error25@mail.ru>
2018-03-21 14:41:32 +0600

    test

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 #ssh deploy@79.137.75.144 'bash -s' < /home/deploy/test_script.sh
-ssh deploy@79.137.75.144 'echo 1 > /home/deploy/test.txt'
+ssh -o "StrictHostKeyChecking no" deploy@79.137.75.144 'echo 1 > /home/deploy/test.txt'
