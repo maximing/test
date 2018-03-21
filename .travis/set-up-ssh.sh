d4982a626a5efb65ce37f82c84661e5527064751
Maxim Ignatov <error25@mail.ru>
2018-03-21 16:10:26 +0600

    t

--- .travis/set-up-ssh.sh
+++ .travis/set-up-ssh.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-openssl aes-256-cbc -K $encrypted_98f936accb6c_key -iv $encrypted_98f936accb6c_iv -in deploy_key.enc -out /tmp/deploy_key -d
+openssl aes-256-cbc -K $encrypted_98f936accb6c_key -iv $encrypted_98f936accb6c_iv -in .travis/deploy_key.enc -out /tmp/deploy_key -d
 eval "$(ssh-agent -s)"
 chmod 600 /tmp/deploy_key
 ssh-add /tmp/deploy_key
-#chmod -R +x .travis
+chmod -R +x .travis
 #mv .travis/deploy_key ~/.ssh/id_rsa
 #cat .travis/ssh_config >> ~/.ssh/config
