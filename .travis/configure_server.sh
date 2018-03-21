ca7414ccefc7e1d50a894b98e8d2209a160f0959
Maxim Ignatov <error25@mail.ru>
2018-03-21 15:16:44 +0600

    test

--- .travis/configure_server.sh
+++ .travis/configure_server.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+#### Install Git ####
+
+#### Install Docker ####
+
 #### Create deploy user ####
 DEPLOY_USER="deploy2"
 DEPLOY_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDJFA9/ot98J4ws2ZzOkT0pbNNqOvR0e6PhoKjKadAEY3mGAsX9nRR1YP4Wvpr80zq3WxjkTQD1Z2j9dE9fxYmJPzpt+O9PIXiv8lE4Y530PGEoEpKpsGckYBn3VksdMCN6Ezv/CaVvwG23jzzuK6kz8lNlO/NwMKsxt3pNBSOaWNZ8Y2Iqgbwl2bg97BOUbMjuHdkYzkhmosjtLA5rXpt9WCVHXT4nYOf9o3ExR1bpNsoZqluWzXrUPZHcl5187IQV0OXAB5eeIQR3vpNoqLWQ3e5fHkKGNDT5xoggoIIvWxkOgYnA3MZeMbEUlP5Qfdq/Qxt6EEuoHRJefIzRbBEZ deploy"
